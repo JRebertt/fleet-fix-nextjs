feca1c4354fae31ac7227ef06fa8426867509965
JRebertt <joaorebertt0219@gmail.com>
2023-12-27 00:30:05 -0300

    add sidebar

--- components/constants/side-nav.tsx
+++ components/constants/side-nav.tsx
@@ -1,4 +1,10 @@
-import { LayoutDashboard, ListTodo, Users } from 'lucide-react'
+import {
+  Building2,
+  FileSpreadsheet,
+  LayoutDashboard,
+  Truck,
+  User,
+} from 'lucide-react'
 import { type NavItem } from '@/@types/types'
 
 export const NavItems: NavItem[] = [
@@ -6,65 +12,30 @@ export const NavItems: NavItem[] = [
     title: 'Dashboard',
     icon: LayoutDashboard,
     href: '/',
-    color: 'text-sky-500',
   },
   {
-    title: 'Users',
-    icon: Users,
-    href: '/users',
-    color: 'text-green-500',
-  },
-  {
-    title: 'TodoList',
-    icon: ListTodo,
-    href: '/todolist',
-    color: 'text-orange-500',
+    title: 'Tabelas',
+    icon: FileSpreadsheet,
+    href: '/vehicles',
     isChidren: true,
     children: [
       {
-        title: 'children1',
-        icon: ListTodo,
-        color: 'text-pink-500',
-        href: '/todolist/children1',
+        title: 'Empresas',
+        icon: Building2,
+
+        href: '/companies',
       },
       {
-        title: 'children2',
-        icon: ListTodo,
-        color: 'text-pink-500',
-        href: '/todolist/children2',
+        title: 'Motoristas',
+        icon: User,
+
+        href: '/drivers',
       },
       {
-        title: 'children3',
-        icon: ListTodo,
-        color: 'text-pink-500',
-        href: '/todolist/children3',
-      },
-    ],
-  },
-  {
-    title: 'Settings',
-    icon: ListTodo,
-    href: '/settings',
-    color: 'text-orange-500',
-    isChidren: true,
-    children: [
-      {
-        title: 'children1',
-        icon: ListTodo,
-        color: 'text-pink-500',
-        href: '/settings/children1',
-      },
-      {
-        title: 'children2',
-        icon: ListTodo,
-        color: 'text-pink-500',
-        href: '/settings/children2',
-      },
-      {
-        title: 'children3',
-        icon: ListTodo,
-        color: 'text-pink-500',
-        href: '/settings/children3',
+        title: 'Veiculos',
+        icon: Truck,
+
+        href: '/vehicles',
       },
     ],
   },
