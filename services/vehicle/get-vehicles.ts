30edec639d3b1e21d81bf6028369b16e03e00bdd
JRebertt <joaorebertt0219@gmail.com>
2024-01-14 02:49:43 -0300

    fix: error fetch add no-store

--- services/vehicle/get-vehicles.ts
+++ services/vehicle/get-vehicles.ts
@@ -2,8 +2,7 @@ import { Vehicle } from '@/@types/vehicle-table'
 
 export default async function getVehicles(): Promise<Vehicle[]> {
   const res = await fetch('http://127.0.0.1:3000/api/vehicles', {
-    method: 'GET',
-    next: { revalidate: 1 },
+    cache: 'no-store',
   })
   const data = await res.json()
 
