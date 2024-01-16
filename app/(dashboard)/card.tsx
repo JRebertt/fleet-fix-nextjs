c6ac8b1e0c0124f3801894894860901f8f963729
JRebertt <joaorebertt0219@gmail.com>
2024-01-16 01:59:18 -0300

    fix: lint format

--- app/(dashboard)/card.tsx
+++ app/(dashboard)/card.tsx
@@ -1,32 +1,32 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardDescription,
-  CardFooter,
-} from '@/components/ui/card'
+// import {
+//   Card,
+//   CardContent,
+//   CardHeader,
+//   CardDescription,
+//   CardFooter,
+// } from '@/components/ui/card'
 
-import { Options } from './options'
-import { ButtonStart } from './button-start'
+// import { Options } from './options'
+// import { ButtonStart } from './button-start'
 
-export function CardComponent() {
-  return (
-    // <Card className="max-h-[20rem] p-4 flex flex-col gap-2 justify-center items-center">
-    //   <CardHeader className="flex flex-row w-full items-center p-0 justify-between">
-    //     <span className="max-w-[6rem] font-semibold">{formattedDateV2}</span>
-    //     <Options value={data} />
-    //   </CardHeader>
-    //   <CardContent className="p-0">
-    //     <div>{data.vehicleId}</div>
-    //     <div>{data.status}</div>
-    //     <div>{data.priority}</div>
-    //   </CardContent>
-    //   <CardDescription className="p-0">{data.description}</CardDescription>
-    //   <CardFooter className="p-0">
-    //     <ButtonStart value={data} />
-    //   </CardFooter>
-    // </Card>
+// export function CardComponent() {
+//   return (
+//     // <Card className="max-h-[20rem] p-4 flex flex-col gap-2 justify-center items-center">
+//     //   <CardHeader className="flex flex-row w-full items-center p-0 justify-between">
+//     //     <span className="max-w-[6rem] font-semibold">{formattedDateV2}</span>
+//     //     <Options value={data} />
+//     //   </CardHeader>
+//     //   <CardContent className="p-0">
+//     //     <div>{data.vehicleId}</div>
+//     //     <div>{data.status}</div>
+//     //     <div>{data.priority}</div>
+//     //   </CardContent>
+//     //   <CardDescription className="p-0">{data.description}</CardDescription>
+//     //   <CardFooter className="p-0">
+//     //     <ButtonStart value={data} />
+//     //   </CardFooter>
+//     // </Card>
 
-    <h3>Hello World</h3>
-  )
-}
+//     <h3>Hello World</h3>
+//   )
+// }
