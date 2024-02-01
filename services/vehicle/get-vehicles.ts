505956490737e57fcf24a1567fb73613bca20467
JRebertt <joaorebertt0219@gmail.com>
2024-02-01 13:10:35 -0300

    fix: delay in vehicles promise

--- services/vehicle/get-vehicles.ts
+++ services/vehicle/get-vehicles.ts
@@ -2,9 +2,8 @@ import { Vehicle } from '@/@types/vehicle-table'
 import { api } from '@/lib/api-fetch'
 
 export default async function getVehicles(): Promise<Vehicle[]> {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
+  // await new Promise((resolve) => setTimeout(resolve, 1000))
   const res = await api(`/vehicles`, {
-    method: 'GET',
     cache: 'no-store',
   })
 
