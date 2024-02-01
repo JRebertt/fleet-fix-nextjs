2f28d17ec4df1e547fe0472f590c32495831a5bc
JRebertt <joaorebertt0219@gmail.com>
2024-02-01 17:47:34 -0300

    fix: delay in vehicles promise

--- app/(tables)/vehicles/page.tsx
+++ app/(tables)/vehicles/page.tsx
@@ -4,9 +4,23 @@ import { columns } from './columns'
 import { Toaster } from '@/components/ui/sonner'
 import VehicleForm from '@/components/vehicle-form'
 import getVehicles from '@/services/vehicle/get-vehicles'
+import { useEffect, useState } from 'react'
+import { z } from 'zod'
+import { vehicleSchema } from '@/schemas/vehicle'
+import { Vehicle } from '@/@types/vehicle-table'
+
+type VehiclesValues = z.infer<typeof vehicleSchema>
 
 export default async function DemoPage() {
-  const vehicles = await getVehicles()
+  const [vehicles, setVehicles] = useState<Vehicle[]>([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const vehicleList = await getVehicles()
+
+      setVehicles(vehicleList)
+    }
+    fetchData()
+  })
 
   return (
     <>
