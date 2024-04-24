a382708f493468c914617edf68b2b25b703ab69c
JRebertt <joaorebertt0219@gmail.com>
2024-04-24 16:05:54 -0300

    feat: incledes the payments informations like maintenance titles

--- @types/payment.d.ts
+++ @types/payment.d.ts
@@ -1,19 +1,46 @@
-export interface Payment {
-  id: string // Adicionado conforme solicitado
-  amount: number
-  description: string | null // 'string | null' reflete 'z.string().nullable()'
-  paymentDate: Date | null // 'Date | null' reflete 'z.date().nullable()'
-  paymentMethod: 'Card' | 'Boleto' | 'Transfer' | 'Cash' | 'Pix' // Enum definido
-  status:
-    | 'Pending'
-    | 'Completed'
-    | 'Canceled'
-    | 'Failed'
-    | 'InProcess'
-    | 'OnHold' // Enum definido
-  maintenance_id: string
+type MaintenanceStatus =
+  | 'Scheduled'
+  | 'InProgress'
+  | 'Completed'
+  | 'OnHold'
+  | 'Canceled'
+  | 'Failed'
+
+type PaymentStatus =
+  | 'Pending'
+  | 'Completed'
+  | 'Canceled'
+  | 'Failed'
+  | 'InProcess'
+  | 'OnHold'
+
+type PaymentMethod = 'Card' | 'Boleto' | 'Transfer' | 'Cash' | 'Pix'
+
+interface Maintenance {
+  id: string
+  title: string
+  scheduledDate: string // Considerando ISO string para datas
+  status: MaintenanceStatus // Enum simplificado, ajuste conforme necessário
+  description: string
+  startDate: string // Considerando ISO string para datas
+  endDate: string // Considerando ISO string para datas
+  cost: number
+  vehicle_id: string
+  created_at: string // Considerando ISO string para datas
+  updated_at: string // Considerando ISO string para datas
 }
 
+export interface Payment {
+  id: string
+  amount: number
+  description: string | null // 'string | null' para descrição opcional
+  paymentDate: Date | null // 'Date | null' para data de pagamento opcional
+  paymentMethod: PaymentMethod // Tipos de métodos de pagamento
+  status: PaymentStatus
+  maintenance_id: string
+
+  maintenance?: Maintenance
+}
 export interface PaymentReponse {
   payments: Payment[]
 }
